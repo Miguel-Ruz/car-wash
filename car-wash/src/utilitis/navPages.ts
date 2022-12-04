fad744ebcf1e26a1f85fb140f74013b2754df2f5
edgar <a.edgar2209@gmail.com>
2022-12-04 18:19:27 -0500

    add/arreglo de objetos con info de rutas

---
+++ car-wash/src/utilitis/navPages.ts
@@ -0,0 +1,32 @@
+
+import dashboard from "../../public/dashboard.svg"
+import calendar from "../../public/calendar.svg"
+import chart from "../../public/chart.svg"
+import users from "../../public/users.svg"
+
+
+
+
+
+export const navPage = [
+  {
+    id: 1,
+    pageName: "Lavados",
+    icon: calendar,
+  },
+  {
+    id: 2,
+    pageName: "Reporte de ventas",
+    icon: chart,
+  },
+  {
+    id: 3,
+    pageName: "Lavadores",
+    icon: users,
+  },
+  {
+    id: 4,
+    pageName: "Dashboard",
+    icon: dashboard,
+  },
+];
