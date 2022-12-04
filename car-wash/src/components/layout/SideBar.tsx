f26cfe1774981d253e6eecfa899b9f172217f853
edgar <a.edgar2209@gmail.com>
2022-12-04 18:19:06 -0500

    add/incorpore informacion e iconos de las pages

--- car-wash/src/components/layout/SideBar.tsx
+++ car-wash/src/components/layout/SideBar.tsx
@@ -1,5 +1,14 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  ListIcon,
+  ListItem,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
+import Image from "next/image";
 import React from "react";
+import logo from "../../../public/logo.svg";
+import { navPage } from "../../utilitis/navPages";
 
 type Props = {};
 
@@ -15,8 +24,34 @@ function SideBar({}: Props) {
       paddingLeft="24px"
       bg="primaryColor"
       borderRight="1px solid #E2E8F0"
+      direction="column"
+      rowGap="64px"
     >
-        
+      <Flex h="3rem" w="11.563rem">
+        <Image src={logo} alt="logo de la app" />
+      </Flex>
+      <UnorderedList
+        listStyleType="none"
+        rowGap="18px"
+        display="flex"
+        flexDirection="column"
+      >
+        {navPage.map((page) => {
+          return (
+            <ListItem
+              key={page.id}
+              display="flex"
+              alignItems="center"
+              columnGap="8px"
+            >
+              <Image src={page.icon} alt="logo" />
+              <Text fontSize="sm" color="fontColor" fontWeight="semibold">
+                {page.pageName}
+              </Text>
+            </ListItem>
+          );
+        })}
+      </UnorderedList>
     </Flex>
   );
 }
