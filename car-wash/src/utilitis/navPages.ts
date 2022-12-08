f3a39599246213963e29b050bf216d9c3dffa811
edgar <a.edgar2209@gmail.com>
2022-12-08 14:49:36 -0500

    add/agregue links al arreglo de pages y lo tipie

--- car-wash/src/utilitis/navPages.ts
+++ car-wash/src/utilitis/navPages.ts
@@ -4,29 +4,40 @@ import calendar from "../../public/calendar.svg"
 import chart from "../../public/chart.svg"
 import users from "../../public/users.svg"
 
+interface INavPages {
+  id: number;
+  pageName: string;
+  icon?:string;
+  link: string;
+}
+/* interface INavPages extends Array<INavPages> {} */
 
 
-
-
-export const navPage = [
+export const navPage: INavPages[] = [
   {
     id: 1,
     pageName: "Lavados",
     icon: calendar,
+    link: "/lavados",
   },
   {
     id: 2,
     pageName: "Reporte de ventas",
     icon: chart,
+    link: "/reportes",
+
   },
   {
     id: 3,
     pageName: "Lavadores",
     icon: users,
+    link: "/lavadores",
+
   },
   {
     id: 4,
     pageName: "Dashboard",
     icon: dashboard,
+    link: "/",
   },
 ];
