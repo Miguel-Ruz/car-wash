f480004a25102be1abad23d5b13d17c5c0cc97e7
edgar <a.edgar2209@gmail.com>
2022-12-15 20:13:36 -0500

    add/maquetacion componente cardInfo dashboard

--- car-wash/src/components/dashboard/CardInfoDashboard.tsx
+++ car-wash/src/components/dashboard/CardInfoDashboard.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { Box, Flex, HStack, Text } from "@chakra-ui/react";
+import React from "react";
 
-type Props = {}
+type Props = {
+  bgColor: string;
+  bgIconColor: string;
+  title: string;
+  amount: number;
+};
 
-const CardInfoDashboard = (props: Props) => {
+const CardInfoDashboard = ({ bgColor, bgIconColor, title, amount }: Props) => {
   return (
-    <div>CardInfoDashboard</div>
-  )
-}
+    <Flex
+      direction="column"
+      padding="16px 24px"
+      bg={bgColor}
+      borderRadius="12px"
+      w="33%"
+      rowGap="16px"
+    >
+      <HStack>
+        <Box
+          w="44px"
+          h="44px"
+          borderRadius="34px"
+          bg={bgIconColor}
+          p="10px"
+          textAlign="center"
+        >
+          i
+        </Box>
+        <Text>{title}</Text>
+      </HStack>
+      {/* logica a cambiar: colocar condicionalmente simbolo de peso */}
+      <Text fontSize="2xl" fontWeight="semibold">
+        {amount.toString().length > 2
+          ? "$" + amount.toLocaleString("es")
+          : amount}
+      </Text>
+    </Flex>
+  );
+};
 
-export default CardInfoDashboard
+export default CardInfoDashboard;
