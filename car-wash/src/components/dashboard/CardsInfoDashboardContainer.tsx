be771fd47cbc9e203b6b2146c07ba2d1eda1dcc1
pdromoreno7 <pdromorenodev@gmail.com>
2023-07-09 17:53:30 -0500

    chage/ prods icon card

--- car-wash/src/components/dashboard/CardsInfoDashboardContainer.tsx
+++ car-wash/src/components/dashboard/CardsInfoDashboardContainer.tsx
@@ -1,6 +1,7 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
 import CardInfoDashboard from "./CardInfoDashboard";
+import { MdCalendarViewDay } from "react-icons/md";
 
 interface CardData {
   bgColor: string;
@@ -37,24 +38,25 @@ function CardsInfoDashboardContainer({
   return (
     <Flex p="0 24px" columnGap="1.25rem" m="2rem 0">
       <CardInfoDashboard
-              bgColor="#F0FFF4"
-              bgIconColor="#C6F6D5"
-              title="Lavados del día"
-              amount={25}
-            />
-            
-            <CardInfoDashboard
-              bgColor="#EBF8FF"
-              bgIconColor="#BEE3F8"
-              title="Lavados del día"
-              amount={1000000}
-            />
-            <CardInfoDashboard
-              bgColor="#FAF5FF"
-              bgIconColor="#E9D8FD"
-              title="Lavados del día"
-              amount={10000000}
-            />
+        bgColor="#F0FFF4"
+        bgIconColor="#C6F6D5"
+        title="Lavados del día"
+        amount={25}
+        iconCard={<MdCalendarViewDay />}
+      />
+
+      <CardInfoDashboard
+        bgColor="#EBF8FF"
+        bgIconColor="#BEE3F8"
+        title="Lavados del día"
+        amount={1000000}
+      />
+      <CardInfoDashboard
+        bgColor="#FAF5FF"
+        bgIconColor="#E9D8FD"
+        title="Lavados del día"
+        amount={10000000}
+      />
     </Flex>
   );
 }
