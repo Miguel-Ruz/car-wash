af2726ab1dd67afdbce2c334c91389ee809aafcf
pdromoreno7 <pdromorenodev@gmail.com>
2023-07-09 14:58:56 -0500

    add/ se añade componente container cards info

--- car-wash/src/pages/index.tsx
+++ car-wash/src/pages/index.tsx
@@ -4,6 +4,28 @@ import { DashboardLayout } from "../components";
 import logo from "../../public/logoDash.png";
 import { CardInfoDashboard, DataTableDashboard } from "../components/dashboard";
 import TopBar from "../components/common/TopBar";
+import CardsInfoDashboardContainer from "../components/dashboard/CardsInfoDashboardContainer";
+
+const data = [
+  {
+    bgColor: "#F0FFF4",
+    bgIconColor: "#C6F6D5",
+    title: "Lavados del día",
+    amount: 25,
+  },
+  {
+    bgColor: "#EBF8FF",
+    bgIconColor: "#BEE3F8",
+    title: "Lavados del día",
+    amount: 25,
+  },
+  {
+    bgColor: "#FAF5FF",
+    bgIconColor: "#E9D8FD",
+    title: "Lavados del día",
+    amount: 25,
+  },
+];
 
 export default function Home() {
   return (
@@ -27,16 +49,20 @@ export default function Home() {
             >
               <Flex direction="column" color="mainColor" rowGap="16px">
                 <Heading size="xl">Mav Wash</Heading>
-                <Text fontSize="lg" color=" rgba(255, 255, 255, 0.8)" fontWeight="semibold">
+                <Text
+                  fontSize="lg"
+                  color=" rgba(255, 255, 255, 0.8)"
+                  fontWeight="semibold"
+                >
                   Estas son las estadisticas
                 </Text>
               </Flex>
 
-              <Image src={logo} alt="logo de la app" priority/>
+              <Image src={logo} alt="logo de la app" priority />
             </Flex>
           </Box>
 
-          <Flex p="0 24px" columnGap="1.25rem">
+          {/* <Flex p="0 24px" columnGap="1.25rem">
             <CardInfoDashboard
               bgColor="#F0FFF4"
               bgIconColor="#C6F6D5"
@@ -55,7 +81,8 @@ export default function Home() {
               title="Lavados del día"
               amount={10000000}
             />
-          </Flex>
+          </Flex> */}
+          <CardsInfoDashboardContainer dataCards={data} />
           <DataTableDashboard />
         </>
       </DashboardLayout>
