9cd51147fb0043b1aa07c68fc65884e201b86d45
rafedev <tecnoportuaria@gmail.com>
2023-11-07 21:21:58 -0500

    cambios x

--- car-wash/src/pages/lavados.tsx
+++ car-wash/src/pages/lavados.tsx
@@ -172,7 +172,7 @@ const lavados = (props: Props) => {
   const listWasher = useFetchData(urlListWasher);
 
   //filtrar por placas
-  const filteredData = listWashes.data?.data?.filter((item) => {
+  const filteredData = listWashes?.data?.data?.filter((item) => {
     const matchesPlacas = item.licensePlate.includes(placasFilter);
     // Obtener el valor seleccionado del Select
     const matchesWashTypes = item.washType.includes(washTypeFilter);
@@ -194,7 +194,7 @@ const lavados = (props: Props) => {
 
     return (
       <Badge variant="solid" colorScheme={statusBadge}>
-        {item.status}
+        {item?.status}
       </Badge>
     );
   };
@@ -205,107 +205,110 @@ const lavados = (props: Props) => {
   };
   return (
     <DashboardLayout>
-      <TopBar title="Lavados" />
-      <CardsInfoDashboardContainer dashboardCounter={data} />
+      <>
+        <TopBar title="Lavados" />
+        <CardsInfoDashboardContainer dashboardCounter={data} />
 
-      <Flex p="32px 24px" justifyContent="space-between">
-        <Flex gap="1rem" alignItems="center">
-          <Text>Buscar</Text>
-          <Input
-            placeholder="Placas"
-            size="md"
-            w="200px"
-            value={placasFilter}
-            onChange={(e) => setPlacasFilter(e.target.value)}
-          />
-          <Select
-            color="fontColorTwo"
-            placeholder="Tipo de lavado"
-            size="md"
-            variant="outline"
-            w="400px"
-            value={washTypeFilter}
-            onChange={(e) => setWashTypeFilter(e.target.value)}
-          >
-            <option value="DRY">DRY</option>
-            <option value="FULL">FULL</option>
-          </Select>
+        <Flex p="32px 24px" justifyContent="space-between">
+          <Flex gap="1rem" alignItems="center">
+            <Text>Buscar</Text>
+            <Input
+              placeholder="Placas"
+              size="md"
+              w="200px"
+              value={placasFilter}
+              onChange={(e) => setPlacasFilter(e.target.value)}
+            />
+            <Select
+              color="fontColorTwo"
+              placeholder="Tipo de lavado"
+              size="md"
+              variant="outline"
+              w="400px"
+              value={washTypeFilter}
+              onChange={(e) => setWashTypeFilter(e.target.value)}
+            >
+              <option value="DRY">DRY</option>
+              <option value="FULL">FULL</option>
+            </Select>
+          </Flex>
+          <Box>
+            <ButtonRegister onOpen={onOpen} title="Registrar nuevo lavado " />
+          </Box>
         </Flex>
-        <Box>
-          <ButtonRegister onOpen={onOpen} title="Registrar nuevo lavado " />
-        </Box>
-      </Flex>
-      <Box px="24px">
-        <TableContainer border="1px solid #E2E8F0" borderRadius="12px">
-          <Table size="md">
-            <Thead>
-              <Tr bg="primaryColor">
-                <Th w={thWidth}>NOMBRE</Th>
-                <Th w={thWidth}>VEHICULO</Th>
-                <Th w={thWidth}>PLACAS</Th>
-                <Th w={thWidth}>LAVADOR</Th>
-                <Th w={thWidth}>LAVADO</Th>
-                <Th w={thWidth}>ESTADO</Th>
+        <Box px="24px">
+          <TableContainer border="1px solid #E2E8F0" borderRadius="12px">
+            <Table size="md">
+              <Thead>
+                <Tr bg="primaryColor">
+                  <Th w={thWidth}>NOMBRE</Th>
+                  <Th w={thWidth}>VEHICULO</Th>
+                  <Th w={thWidth}>PLACAS</Th>
+                  <Th w={thWidth}>LAVADOR</Th>
+                  <Th w={thWidth}>LAVADO</Th>
+                  <Th w={thWidth}>ESTADO</Th>
 
-                <Th />
-              </Tr>
-            </Thead>
-            <Tbody>
-              {filteredData?.map((item) => (
-                <Tr key={item.id}>
-                  <Td>
-                    <WrapItem alignItems="center">
-                      <Avatar
-                        name="Dan Abrahmov"
-                        src="https://bit.ly/dan-abramov"
-                        mr="12px"
-                      />
-                      {item.clientName}
-                    </WrapItem>
-                  </Td>
-                  <Td>{item.vehicleType}</Td>
-                  <Td>{item.licensePlate}</Td>
-                  <Td>{item.washer.name}</Td>
-                  <Td>{item.washType}</Td>
-                  <Td>
-                    <BadgeStatus status={item.status} item={item} />
-                  </Td>
-                  <Td>
-                    <Tooltip hasArrow label="Cerrar día" placement="auto">
-                      <Stack align="end">
-                        <FiCalendar
-                          onClick={}
-                          cursor="pointer"
-                          color="#319795"
-                        />{" "}
-                        {/* ALINEAR A LA DERECHA ESTE BENDITO ICONITO. ME HIZO BOTAR EL CHUPO */}
-                      </Stack>
-                    </Tooltip>
-                  </Td>
+                  <Th />
                 </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-      <ModalGlobal handleModalClose={handleModalClose} isOpen={isOpen}>
-        {hasValuesOnObject(wash) ? (
-          <SuccessData handleModalClose={handleModalClose} />
-        ) : (
-          <ModalAddWash
-            handleModalClose={handleModalClose}
-            setStepperStep={setStepperStep}
-            stepperStep={stepperStep}
-            handleChangeCreateWash={handleChangeCreateWash}
-            createWash={createWash}
-            listWasher={listWasher}
-            isButtonDisabled={isButtonDisabled}
-            handleSubmitCreateWash={handleSubmitCreateWash}
-            loading={loading}
-            isButtonDisabledWashData={isButtonDisabledWashData}
-          />
-        )}
-      </ModalGlobal>
+              </Thead>
+              <Tbody>
+                {filteredData &&
+                  filteredData?.map((item) => (
+                    <Tr key={item.id}>
+                      <Td>
+                        <WrapItem alignItems="center">
+                          <Avatar
+                            name="Dan Abrahmov"
+                            src="https://bit.ly/dan-abramov"
+                            mr="12px"
+                          />
+                          {item?.clientName}
+                        </WrapItem>
+                      </Td>
+                      <Td>{item?.vehicleType}</Td>
+                      <Td>{item?.licensePlate}</Td>
+                      <Td>{item?.washer.name}</Td>
+                      <Td>{item?.washType}</Td>
+                      <Td>
+                        <BadgeStatus status={item?.status} item={item} />
+                      </Td>
+                      <Td>
+                        <Tooltip hasArrow label="Cerrar día" placement="auto">
+                          <Stack align="end">
+                            <FiCalendar
+                              onClick={onOpen}
+                              cursor="pointer"
+                              color="#319795"
+                            />{" "}
+                            {/* ALINEAR A LA DERECHA ESTE BENDITO ICONITO. ME HIZO BOTAR EL CHUPO */}
+                          </Stack>
+                        </Tooltip>
+                      </Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+        <ModalGlobal handleModalClose={handleModalClose} isOpen={isOpen}>
+          {hasValuesOnObject(wash) ? (
+            <SuccessData handleModalClose={handleModalClose} />
+          ) : (
+            <ModalAddWash
+              handleModalClose={handleModalClose}
+              setStepperStep={setStepperStep}
+              stepperStep={stepperStep}
+              handleChangeCreateWash={handleChangeCreateWash}
+              createWash={createWash}
+              listWasher={listWasher}
+              isButtonDisabled={isButtonDisabled}
+              handleSubmitCreateWash={handleSubmitCreateWash}
+              loading={loading}
+              isButtonDisabledWashData={isButtonDisabledWashData}
+            />
+          )}
+        </ModalGlobal>
+      </>
     </DashboardLayout>
   );
 };
